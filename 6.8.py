f37e460c0c0d54c48dcf8ec3c3c4cf5b6d2869f4
uuuumy <e.idanova@g.nsu.ru>
2025-12-02 01:46:57 +0700

    Update 6.8.py

--- 6.8.py
+++ 6.8.py
@@ -1,3 +1,33 @@
-x=int(input())
-r=''.join(str(i) for i in range(201))
-print(r[x-1])
+n = int(input('Hомер позиции: '))
+if n == 1:
+    print(0)
+else:
+    pos = 1
+    for num in range(1, 201):
+        if num < 10:        
+            pos += 1
+            if pos == n:
+                print(num)
+                break
+        elif num < 100:         
+            pos += 1
+            if pos == n:
+                print(num // 10)
+                break
+            pos += 1
+            if pos == n:
+                print(num % 10)
+                break
+        else:                  
+            pos += 1
+            if pos == n:
+                print(num // 100)
+                break
+            pos += 1
+            if pos == n:
+                print((num // 10) % 10)
+                break
+            pos += 1
+            if pos == n:
+                print(num % 10)
+                break
