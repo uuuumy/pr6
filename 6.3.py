3d4eba0863078c930ba202049188b4b64a2c0ada
uuuumy <e.idanova@g.nsu.ru>
2025-12-02 10:47:05 +0700

    6.3.py

--- 6.3.py
+++ 6.3.py
@@ -1,6 +1,7 @@
-N,M=map(int,input().split())
-K=int(input())
-if N+M==K:
+n,m=map(int,input().split())
+k=int(input())
+if k< n*m and (k%n==0 or k%m==0):
     print('успешно')
 else:
+
     print('неосуществимо')
